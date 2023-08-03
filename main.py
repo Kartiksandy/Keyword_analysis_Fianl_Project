5f36a114ab097bd2bece4241e12a534ab8cee62f
Kartiksandy <138625825+Kartiksandy@users.noreply.github.com>
2023-08-03 19:21:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def adScraper(numberOfTimes, listOfKeywords):
     resultDict = {}
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_locations = os.environ.gey("GOOGLE_CHROME_BIN")
+    chrome_options.binary_locations = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_option.add_argument("--disable--dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
