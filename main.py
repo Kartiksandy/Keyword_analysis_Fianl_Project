260c9a11254c5c9d8739ed6739ae5e969d2f12be
Kartiksandy <138625825+Kartiksandy@users.noreply.github.com>
2023-08-04 06:41:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -86,13 +86,13 @@ def adScraper(numberOfTimes, listOfKeywords):
     resultDict = {}
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_locations = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    driver.get("http://www.google.com")
-    print(driver.page_source)
+    driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver.get("http://www.python.org")
+    print(driver.title)
     timeout_value = None
     # Create the WebDriver with the specified timeout
     driver = webdriver.Chrome(
